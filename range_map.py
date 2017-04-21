48ff2a15851a15da272b90eed230d578556550c8
betterleo <sunsap@vip.qq.com>
2017-04-21 11:28:28 +0800

    lambda for range and map

---
+++ range_map.py
@@ -0,0 +1,4 @@
+print lambda x: x * x, range(1, 11)
+#<function <lambda> at 0x00000000025BEF98> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print map(lambda x: x * x, range(1, 11))
+#[1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
